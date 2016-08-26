04e1a8a83b0b7253e45b4bebf46c62fc4b48131d
Chris Kolosiwsky <ckolos@gmail.com>
2016-08-26 15:43:50 -0500

    add an exec to the start up script
    pass START arg to circle test
    
    missing space; added

--- startup.sh
+++ startup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 test -n "$1" && ARG=$(echo $1 | tr [a-z] [A-Z])
-if [[ "$ARG" == "START" ]]; then
-  test -x /usr/local/bin/uwsgi && \
-      /usr/local/bin/uwsgi --ini-paste shavar.ini --paste-logger --uid 10001 --gid 10001
+if [ "$ARG" == "START" ]; then
+  if [ -x /usr/local/bin/uwsgi ]; then
+      exec /usr/local/bin/uwsgi --ini-paste shavar.ini --paste-logger --uid 10001 --gid 10001
+  else
+    echo "uwsgi is not installed"
+    exit
+  fi
 else
   echo "Invalid option specified: ARG=$ARG"
   exit
